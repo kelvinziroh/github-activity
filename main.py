3cacbb858bea651120530a2d2753110163f1256d
Kelvin Ziro <kelvin.ziroh@gmail.com>
2026-06-18 13:44:11 +0300

    Structure response output

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import pprint
 
 import requests
@@ -8,9 +9,15 @@ def main():
     response = requests.get(
         f"https://api.github.com/users/{username}/events",
         headers={"accept": "application/vnd.github+json"},
-    )
+    ).text
+    event_data = json.loads(response)
     print(f"{username}'s activity:")
-    pprint.pprint(response.text, indent=4)
+    for event in event_data:
+        print(f"event id: {event['id']}")
+        print(f"event type: {event['type']}")
+        print(f"repo: {event['repo']['name']}")
+        print(f"public: {event['public']}")
+        print(f"created_at: {event['created_at']}\n")
 
 
 if __name__ == "__main__":
