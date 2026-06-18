a5364ed55ba089913c98174ca95a8cd151c7b8f4
Kelvin Ziro <kelvin.ziroh@gmail.com>
2026-06-18 14:01:32 +0300

    Format datetime string to local date representation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import pprint
+from datetime import datetime
 
 import requests
 
@@ -17,7 +18,14 @@ def main():
         print(f"event type: {event['type']}")
         print(f"repo: {event['repo']['name']}")
         print(f"public: {event['public']}")
-        print(f"created_at: {event['created_at']}\n")
+        print(f"created_at: {format_date(event['created_at'])}\n")
+
+
+def format_date(date_str: str):
+    iso_str = date_str.replace("Z", "+00:00")
+    dt_utc = datetime.fromisoformat(iso_str)
+    dt_local = dt_utc.astimezone()
+    return dt_local.strftime("%Y-%m-%d %H:%M:%S%z")
 
 
 if __name__ == "__main__":
