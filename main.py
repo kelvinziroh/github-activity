4f044a3c7b4b69b6cc3cb91b8bfebe868a74257c
Kelvin Ziro <kelvin.ziroh@gmail.com>
2026-06-20 22:35:35 +0300

    Handle request errors

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import requests
 def main():
     username = get_username()
     event_data = get_data(username)
-    render_activity(username, event_data)
+    if event_data is not None:
+        render_activity(username, event_data)
 
 
 def render_activity(username: str, event_data: list):
@@ -63,12 +64,22 @@ def get_username() -> str:
     return args.username
 
 
-def get_data(username) -> list[dict]:
-    data = requests.get(
-        f"https://api.github.com/users/{username}/events",
-        headers={"accept": "application/vnd.github+json"},
-    ).json()
-    return data
+def get_data(username) -> list[dict] | None:
+    try:
+        data = requests.get(
+            f"https://api.github.com/users/{username}/events",
+            headers={"accept": "application/vnd.github+json"},
+        )
+        data.raise_for_status()
+        return data.json()
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.ConnectionError as conn_err:
+        print(f"Connection error occurred: {conn_err}")
+    except requests.exceptions.Timeout as timeout_err:
+        print(f"Timeout error occurred: {timeout_err}")
+    except requests.exceptions.RequestException as req_err:
+        print(f"An unexpected error occurred: {req_err}")
 
 
 def format_date(date_str: str) -> str:
