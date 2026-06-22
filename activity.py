fee41282e72b8a536793e91fe824743b9840f26f
Kelvin Ziro <kelvin.ziroh@gmail.com>
2026-06-22 21:55:59 +0300

    Refactor code

--- activity.py
+++ activity.py
@@ -1,5 +1,6 @@
-import pprint
 from datetime import datetime
+
+import requests
 
 
 def render_activity(username: str, activity_log: list, activity_stats: dict[str, int]):
@@ -85,3 +86,21 @@ def format_date(date_str: str) -> str:
     dt_utc = datetime.fromisoformat(iso_str)
     dt_local = dt_utc.astimezone()
     return dt_local.strftime("%Y-%m-%d %H:%M:%S%z")
+
+
+def get_data(username: str) -> list[dict] | None:
+    try:
+        data = requests.get(
+            f"https://api.github.com/users/{username}/events",
+            headers={"accept": "application/vnd.github+json"},
+        )
+        data.raise_for_status()
+        return data.json()
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.ConnectionError as conn_err:
+        print(f"Connection error occurred: {conn_err}")
+    except requests.exceptions.Timeout as timeout_err:
+        print(f"Timeout error occurred: {timeout_err}")
+    except requests.exceptions.RequestException as req_err:
+        print(f"An unexpected error occurred: {req_err}")
--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from datetime import datetime
 
 import requests
 
-from activity import get_activity, render_activity
+from activity import get_activity, get_data, render_activity
 
 
 def main():
     username = get_username()
-    event_data = get_data(username)
-    if event_data is not None:
-        activity_log, activity_stats = get_activity(event_data)
+    activity_data = get_data(username)
+    if activity_data is not None:
+        activity_log, activity_stats = get_activity(activity_data)
         render_activity(username, activity_log, activity_stats)
 
 
@@ -19,24 +19,6 @@ def get_username() -> str:
     parser.add_argument("username", help="The handle for the GitHub user account")
     args = parser.parse_args()
     return args.username
-
-
-def get_data(username: str) -> list[dict] | None:
-    try:
-        data = requests.get(
-            f"https://api.github.com/users/{username}/events",
-            headers={"accept": "application/vnd.github+json"},
-        )
-        data.raise_for_status()
-        return data.json()
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-    except requests.exceptions.ConnectionError as conn_err:
-        print(f"Connection error occurred: {conn_err}")
-    except requests.exceptions.Timeout as timeout_err:
-        print(f"Timeout error occurred: {timeout_err}")
-    except requests.exceptions.RequestException as req_err:
-        print(f"An unexpected error occurred: {req_err}")
 
 
 if __name__ == "__main__":
